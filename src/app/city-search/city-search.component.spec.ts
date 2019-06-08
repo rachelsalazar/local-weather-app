88c2102d9d3443b87c7828601fc4c0e51a443753
Rachel Salazar <ret1992@gmail.com>
2019-06-08 10:11:33 -0700

    adding unit testing

--- src/app/city-search/city-search.component.spec.ts
+++ src/app/city-search/city-search.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CitySearchComponent } from './city-search.component';
+import {MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+
 
 describe('CitySearchComponent', () => {
   let component: CitySearchComponent;
@@ -8,6 +13,18 @@ describe('CitySearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        HttpClientTestingModule
+      ],
       declarations: [ CitySearchComponent ]
     })
     .compileComponents();
