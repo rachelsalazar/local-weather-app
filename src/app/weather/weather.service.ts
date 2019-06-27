f86844a06275bc99e1e852f9b9f1e7edcec26e6b
Rachel Salazar <ret1992@gmail.com>
2019-06-26 18:21:10 -0700

    images WORKING

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -52,11 +52,12 @@ export class WeatherService implements IWeatherService {
   }
 
   private transformToICurrentWeather(data: ICurrentWeatherData) : ICurrentWeather{
+    console.log(data.weather[0].icon)
     return {
       city: data.name,
       country: data.sys.country,
       date: data.dt * 1000,
-      image: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
+      image: data.weather[0].icon,
       temperature: Math.floor(data.main.temp),
       description: data.weather[0].description
     }
